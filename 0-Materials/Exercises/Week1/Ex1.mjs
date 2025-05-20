4b3103ab52e62b03f212207f844a96e1ff1834ef
Francesco Re <francesco.re2603@gmail.com>
2025-05-20 10:52:45 +0200

    Started exercises, week 1 es 1 almost done

---
+++ 0-Materials/Exercises/Week1/Ex1.mjs
@@ -0,0 +1,22 @@
+"use strict";
+
+function convertionToCapitals(names)
+{
+    let nameList = names.split(",");
+    let result = [];
+    for ( fullName in nameList )
+    {
+        let words = fullName.split(" ");
+        let r = "";
+        for ( word in words )
+        {
+            r = r + word[0];
+        }
+        result.push(r);
+    }
+    return result.sort();
+}
+
+
+let string = "Luigi De Russis, Francesca Russo, Fulvio Corno, Luca Scibetta, Enrico Masala, Antonio Servetti";
+let result = convertionToCapitals(string);
